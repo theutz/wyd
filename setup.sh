9bf937d89320caf49c3bd9e409ca70690f6b3124
Michael Utz <michael@theutz.com>
2024-08-26 11:58:00 +0300

    chore: add more tools
    Signed-off-by: Michael Utz <michael@theutz.com>

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 if ! command -v asdf 2>/dev/null 1>/dev/null; then
   echo "Please install the asdf version manager. https://asdf-vm.com"
   exit 1
 fi
 
-asdf plugin add golang
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
 asdf plugin add just https://github.com/olofvndrhr/asdf-just.git
 asdf plugin add sqlc https://github.com/dylanrayboss/asdf-sqlc.git
 asdf plugin add goose https://github.com/samhvw8/asdf-goose.git
+asdf plugin add gum https://github.com/lwiechec/asdf-gum.git
+asdf plugin add sqlite https://github.com/cLupus/asdf-sqlite.git
 
 asdf install
 
-echo
-echo "Setup complete!"
+gum format "# ✅ Setup complete!"
