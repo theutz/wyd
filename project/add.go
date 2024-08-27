4582b34cf145661cfa8d27005a153bf431e4855a
Michael Utz <michael@theutz.com>
2024-08-27 16:23:03 +0300

    chore: don't allow client id
    Signed-off-by: Michael Utz <michael@theutz.com>

--- project/add.go
+++ project/add.go
@@ -7,22 +7,19 @@ import (
 )
 
 type AddCmd struct {
-	Name     string `short:"n" help:"the name of the package"`
-	ClientId int64  `xor:"client" help:"the client id (can't be used with --client)"`
-	Client   string `short:"c" xor:"client" help:"the name of the client (can't be used with --client-id)"`
+	Name   string `short:"n" help:"the name of the package"`
+	Client string `short:"c" help:"the name of the client"`
 }
 
 func (cmd *AddCmd) Run(b bindings.Bindings) error {
 	b.Logger.Debug("adding project")
 	b.Logger.Debug("flag", "name", cmd.Name)
-	b.Logger.Debug("flag", "clientId", cmd.ClientId)
 	b.Logger.Debug("flag", "client", cmd.Client)
 
 	fields := []huh.Field{}
 
 	params := queries.CreateProjectParams{
-		Name:     cmd.Name,
-		ClientID: int64(cmd.ClientId),
+		Name: cmd.Name,
 	}
 
 	if cmd.Name == "" {
@@ -33,7 +30,7 @@ func (cmd *AddCmd) Run(b bindings.Bindings) error {
 		fields = append(fields, name)
 	}
 
-	if cmd.ClientId == 0 && cmd.Client == "" {
+	if cmd.Client == "" {
 		b.Logger.Debug("client is empty. prompting for input.")
 		b.Logger.Debug("loading clients")
 		clients, err := b.Queries.ListClients(b.Context)
@@ -56,9 +53,7 @@ func (cmd *AddCmd) Run(b bindings.Bindings) error {
 			Value(&params.ClientID)
 		b.Logger.Debug("field", "client", client)
 		fields = append(fields, client)
-	}
-
-	if params.ClientID == 0 {
+	} else {
 		b.Logger.Debug("searching for client by name", "name", cmd.Client)
 		client, err := b.Queries.GetClientByName(b.Context, cmd.Client)
 		if err != nil {
