5e3f5b6717842cd380fa5251ccd6d19219d5ab2c
Michael Utz <michael@theutz.com>
2024-08-27 16:46:54 +0300

    refactor: cleanup logging
    Signed-off-by: Michael Utz <michael@theutz.com>

--- project/add.go
+++ project/add.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/log"
 	"github.com/theutz/wyd/bindings"
 	"github.com/theutz/wyd/queries"
 )
@@ -12,6 +13,7 @@ type AddCmd struct {
 	fields []huh.Field
 	params queries.CreateProjectParams
 	b      bindings.Bindings
+	log    log.Logger
 }
 
 func (cmd *AddCmd) handleName() {
@@ -19,7 +21,7 @@ func (cmd *AddCmd) handleName() {
 		name := huh.NewInput().
 			Title("Name").
 			Value(&cmd.params.Name)
-		cmd.b.Logger.Debug("name is empty. prompting for input", "field", name)
+		cmd.log.Debug("name is empty. prompting for input")
 		cmd.fields = append(cmd.fields, name)
 	} else {
 		cmd.params.Name = cmd.Name
@@ -28,46 +30,45 @@ func (cmd *AddCmd) handleName() {
 
 func (cmd *AddCmd) handleClient() {
 	if cmd.Client == "" {
-		cmd.b.Logger.Debug("client is empty. prompting for input.")
-		cmd.b.Logger.Debug("loading clients")
+		cmd.log.Debug("client is empty. prompting for input.")
+		cmd.log.Debug("loading clients")
 		clients, err := cmd.b.Queries.ListClients(cmd.b.Context)
 		if err != nil {
-			cmd.b.Logger.Fatal(err)
+			cmd.log.Fatal(err)
 		}
-		cmd.b.Logger.Debug("loaded clients", "clients", clients)
+		cmd.log.Debug("loaded clients", "count", len(clients))
 
-		cmd.b.Logger.Debug("converting clients to options")
+		cmd.log.Debug("converting clients to options")
 		var options []huh.Option[int64]
 		for _, c := range clients {
 			o := huh.NewOption[int64](c.Name, c.ID)
 			options = append(options, o)
 		}
-		cmd.b.Logger.Debug("converted clients to options", "options", options)
+		cmd.log.Debug("converted clients to options", "options", options)
 
 		client := huh.NewSelect[int64]().
 			Title("Client").
 			Options(options...).
 			Value(&cmd.params.ClientID)
-		cmd.b.Logger.Debug("field", "client", client)
 		cmd.fields = append(cmd.fields, client)
 	} else {
-		cmd.b.Logger.Debug("searching for client by name", "name", cmd.Client)
+		cmd.log.Debug("searching for client by name", "name", cmd.Client)
 		client, err := cmd.b.Queries.GetClientByName(cmd.b.Context, cmd.Client)
 		if err != nil {
-			cmd.b.Logger.Fatal(err)
+			cmd.log.Fatal(err)
 		}
-		cmd.b.Logger.Debug("found", "client", client)
+		cmd.log.Debug("found", "client", client)
 		cmd.params.ClientID = client.ID
 	}
 }
 
 func (cmd *AddCmd) saveProject() {
-	cmd.b.Logger.Debug("creating project", "params", cmd.params)
+	cmd.log.Debug("creating project", "params", cmd.params)
 	project, err := cmd.b.Queries.CreateProject(cmd.b.Context, cmd.params)
 	if err != nil {
-		cmd.b.Logger.Fatal(err)
+		cmd.log.Fatal(err)
 	}
-	cmd.b.Logger.Info("project created", "project", project)
+	cmd.log.Info("project created", "project", project)
 }
 
 func (cmd *AddCmd) runForm() {
@@ -77,16 +78,18 @@ func (cmd *AddCmd) runForm() {
 		)
 		err := form.Run()
 		if err != nil {
-			cmd.b.Logger.Fatal(err)
+			cmd.log.Fatal(err)
 		}
 	}
 }
 
 func (cmd *AddCmd) Run(b bindings.Bindings) error {
 	cmd.b = b
-	b.Logger.Debug("adding project")
-	b.Logger.Debug("flag", "name", cmd.Name)
-	b.Logger.Debug("flag", "client", cmd.Client)
+	cmd.log = *b.Logger
+
+	cmd.log.Debug("adding project")
+	cmd.log.Debug("flag", "name", cmd.Name)
+	cmd.log.Debug("flag", "client", cmd.Client)
 
 	cmd.handleName()
 	cmd.handleClient()
