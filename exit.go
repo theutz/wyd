bad382c76dc765c79b0df30321e97b8a2bbcc528
Michael Utz <michael@theutz.com>
2024-09-04 22:54:27 +0300

    feat: restart
    Signed-off-by: Michael Utz <michael@theutz.com>

--- exit.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-// StatusAborted is the exit code for aborted commands.
-const StatusAborted = 130
-
-// ErrAborted is the error to return when a command is aborted by Ctrl + C
-var ErrAborted = fmt.Errorf("aborted")
