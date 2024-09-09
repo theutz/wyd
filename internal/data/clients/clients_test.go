64fe2ebc881412db368fe907ebc004f166a85bbf
Michael Utz <michael@theutz.com>
2024-09-09 18:56:21 +0300

    feat(clients): list clients

--- internal/data/clients/clients_test.go
+++ internal/data/clients/clients_test.go
@@ -2,19 +2,28 @@ package clients
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
 	"github.com/theutz/wyd/internal/db"
 )
 
-func TestAddClient(t *testing.T) {
-	// Arrange
+func makeQ(t *testing.T) (context.Context, *sql.DB, *Queries) {
+	t.Helper()
+
 	ctx := context.Background()
 	db, err := db.New(ctx, ":memory:")
 	assert.NoError(t, err)
-	defer db.Close()
 	q := New(db)
+
+	return ctx, db, q
+}
+
+func TestAddClient(t *testing.T) {
+	// Arrange
+	ctx, db, q := makeQ(t)
+	defer db.Close()
 
 	// Act
 	c, err := q.AddClient(ctx, "delegator")
@@ -22,4 +31,52 @@ func TestAddClient(t *testing.T) {
 
 	// Assert
 	assert.Equal(t, "delegator", c.Name)
+}
+
+func TestListClients(t *testing.T) {
+	testCases := []struct {
+		name         string
+		client_names []string
+		wants        []Client
+	}{
+		{
+			name:         "no clients",
+			client_names: []string{},
+			wants:        nil,
+		},
+		{
+			name:         "one client",
+			client_names: []string{"Delegator"},
+			wants:        []Client{{ID: 1, Name: "Delegator"}},
+		},
+		{
+			name:         "multiple clients",
+			client_names: []string{"Huey", "Dewy", "Louis"},
+			wants: []Client{
+				{ID: 1, Name: "Huey"},
+				{ID: 2, Name: "Dewy"},
+				{ID: 3, Name: "Louis"},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			ctx, db, q := makeQ(t)
+			defer db.Close()
+			for _, c := range tc.client_names {
+				_, err := q.AddClient(ctx, c)
+				assert.NoError(t, err)
+			}
+
+			// Act
+			c, err := q.ListClients(ctx)
+			assert.NoError(t, err)
+
+			// Assert
+			assert.Equal(t, tc.wants, c)
+			assert.Compare(t, tc.wants, c)
+		})
+	}
 }
--- internal/data/clients/query.sql
+++ internal/data/clients/query.sql
@@ -2,3 +2,7 @@
 INSERT INTO clients (name)
 VALUES (?)
 RETURNING *;
+
+-- name: ListClients :many
+SELECT *
+FROM clients;
