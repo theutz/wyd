2b54ca6083a2040dc7cbd7c2a454dc1cd44c8161
Michael Utz <michael@theutz.com>
2024-09-11 15:37:57 +0300

    ci: better mage test

--- magefiles/test.go
+++ magefiles/test.go
@@ -9,9 +9,43 @@ type Test mg.Namespace
 
 // run all tests
 func (Test) All() error {
+	mg.Deps(Tidy)
+
 	err := sh.RunV("gotestsum", "./...")
 	if err != nil {
 		return mg.Fatal(1, err)
 	}
+
+	return nil
+}
+
+func Generate() error {
+	args := []string{"generate"}
+	if mg.Verbose() {
+		args = append(args, "-v")
+	}
+	args = append(args, "./...")
+
+	err := sh.Run("go", args...)
+	if err != nil {
+		return mg.Fatal(1, err)
+	}
+
+	return nil
+}
+
+func Tidy() error {
+	mg.Deps(Generate)
+
+	args := []string{"mod", "tidy"}
+	if mg.Verbose() {
+		args = append(args, "-v")
+	}
+
+	err := sh.Run("go", args...)
+	if err != nil {
+		return mg.Fatal(1, err)
+	}
+
 	return nil
 }
