ef0f8a296423a3684b046e8e659c7c03517f7e54
Michael Utz <michael@theutz.com>
2024-09-13 13:15:45 +0300

    test: clients
    Signed-off-by: Michael Utz <michael@theutz.com>

--- internal/db/clients_test.go
+++ internal/db/clients_test.go
@@ -2,81 +2,69 @@ package db
 
 import (
 	"context"
-	"database/sql"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
+	"github.com/jaswdr/faker"
 	"github.com/theutz/wyd/internal/db/queries"
 )
 
-func makeQ(t *testing.T) (context.Context, *sql.DB, *queries.Queries) {
+func mkClients(t *testing.T, count int) (context.Context, *queries.Queries, []queries.Client) {
 	t.Helper()
+	ctx, q := mkQ(t)
 
-	ctx := context.Background()
-	db, err := New(ctx, ":memory:")
-	assert.NoError(t, err)
-	q := queries.New(db)
+	clients := []queries.Client{}
+	faker := faker.New()
 
-	return ctx, db, q
+	for i := 0; i < count; i++ {
+		name := faker.Person().Name()
+
+		c, err := q.AddClient(ctx, name)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		clients = append(clients, c)
+	}
+
+	return ctx, q, clients
 }
 
 func TestAddClient(t *testing.T) {
 	// Arrange
-	ctx, db, q := makeQ(t)
-	defer db.Close()
+	ctx, q := mkQ(t)
 
 	// Act
-	c, err := q.AddClient(ctx, "delegator")
-	assert.NoError(t, err)
+	client, err := q.AddClient(ctx, "delegator")
 
 	// Assert
-	assert.Equal(t, "delegator", c.Name)
+	assert.NoError(t, err)
+	assert.Equal(t, "delegator", client.Name)
 }
 
 func TestListClients(t *testing.T) {
-	testCases := []struct {
-		name         string
-		client_names []string
-		wants        []queries.Client
-	}{
-		{
-			name:         "no clients",
-			client_names: []string{},
-			wants:        nil,
-		},
-		{
-			name:         "one client",
-			client_names: []string{"Delegator"},
-			wants:        []queries.Client{{ID: 1, Name: "Delegator"}},
-		},
-		{
-			name:         "multiple clients",
-			client_names: []string{"Huey", "Dewy", "Louis"},
-			wants: []queries.Client{
-				{ID: 1, Name: "Huey"},
-				{ID: 2, Name: "Dewy"},
-				{ID: 3, Name: "Louis"},
-			},
-		},
-	}
+	// Arrange
+	ctx, q, _ := mkClients(t, 5)
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			// Arrange
-			ctx, db, q := makeQ(t)
-			defer db.Close()
-			for _, c := range tc.client_names {
-				_, err := q.AddClient(ctx, c)
-				assert.NoError(t, err)
-			}
+	// Act
+	clients, err := q.ListClients(ctx)
 
-			// Act
-			c, err := q.ListClients(ctx)
-			assert.NoError(t, err)
+	// Assert
+	assert.NoError(t, err)
+	assert.Equal(t, 5, len(clients))
+}
 
-			// Assert
-			assert.Equal(t, tc.wants, c)
-			assert.Compare(t, tc.wants, c)
-		})
-	}
+func TestDeleteClient(t *testing.T) {
+	// Arrange
+	count := 20
+	ctx, q, clients := mkClients(t, count)
+
+	// Act
+	_, err := q.DeleteClient(ctx, clients[1].Name)
+
+	// Assert
+	assert.NoError(t, err)
+	clients, err = q.ListClients(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, count-1, len(clients))
 }
