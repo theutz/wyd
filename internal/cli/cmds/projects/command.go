c83548b175f3851e132c6ab5588e82848f6d411a
Michael Utz <michael@theutz.com>
2024-09-14 01:54:39 +0300

    chore: remove unreachable code
    Signed-off-by: Michael Utz <michael@theutz.com>

--- internal/cli/cmds/projects/command.go
+++ internal/cli/cmds/projects/command.go
@@ -2,15 +2,12 @@ package projects
 
 func (cmd *AddProjectsCmd) Run() error {
 	panic("unimplemented")
-	return nil
 }
 
 func (cmd *ListProjectsCmd) Run() error {
 	panic("unimplemented")
-	return nil
 }
 
 func (cmd *DeleteProjectsCmd) Run() error {
 	panic("unimplemented")
-	return nil
 }
--- internal/cli/cmds/tasks/commands.go
+++ internal/cli/cmds/tasks/commands.go
@@ -2,20 +2,16 @@ package tasks
 
 func (cmd *TasksCmd) Run() error {
 	panic("unimplemented")
-	return nil
 }
 
 func (cmd *AddTaskCmd) Run() error {
 	panic("unimplemented")
-	return nil
 }
 
 func (cmd *ListTasksCmd) Run() error {
 	panic("unimplemented")
-	return nil
 }
 
 func (cmd *DeleteTaskCmd) Run() error {
 	panic("unimplemented")
-	return nil
 }
