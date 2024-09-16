33840de0b979e0d95b613edcf7d6b9b2651e3d20
Michael Utz <michael@theutz.com>
2024-09-16 22:13:48 +0300

    chore: basic snapshots setup
    Signed-off-by: Michael Utz <michael@theutz.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/theutz/wyd
 
 go 1.22.6
+
+require github.com/bradleyjkemp/cupaloy v2.3.0+incompatible
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
---
+++ main_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/bradleyjkemp/cupaloy"
+)
+
+func CaptureOutput(t *testing.T, f func()) string {
+	t.Helper()
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		err = fmt.Errorf("error: setting up pipe: %w", err)
+		t.Fatal(err)
+	}
+	defer r.Close()
+
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+	}()
+	os.Stdout = w
+	os.Stderr = w
+
+	f()
+	w.Close()
+
+	s := strings.Builder{}
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		s.WriteString(fmt.Sprintf("%s\n", scanner.Text()))
+	}
+	if scanner.Err() != nil {
+		err = fmt.Errorf("error: while scanning output: %w", err)
+		t.Fatal(err)
+	}
+	out := s.String()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return out
+}
+
+func TestInit(t *testing.T) {
+	testCases := []struct {
+		name     string
+		args     []string
+		exitCode int
+	}{
+		{"no args", []string{}, 1},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+
+			// Act
+			out := CaptureOutput(t, func() {
+				Init()
+			})
+
+			// Assert
+			cupaloy.SnapshotT(t, out)
+		})
+	}
+}
